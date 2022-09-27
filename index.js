6918a31f5fe7f9dab96fe4bfb06a2c56a35c0552
Mauro_Difilippo <maurodifi16@gmail.com>
2022-09-27 18:29:47 -0300

    Modificaciones

--- index.js
+++ index.js
@@ -11,24 +11,26 @@ const app = express();
 //express-hbs config
 app.engine("hbs", hbs.engine({extname: "hbs"}));
 app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "/views"))
+app.set("views", path.join(__dirname, "/views"));
 
+app.use(express.static(path.join(__dirname, "/public")));
+/* app.use(express.static('public')); */
 app.use(express.json());
 //habilitamos la lectura de datos del body de la request
 app.use(express.urlencoded({ extended: false}));
 
 app.get("/", (req, res) =>{
     res.render("home");
-})
+});
 
 app.use("/racers", (req, res) =>{
     res.render("racers")
-})
+});
 app.use("/teams", (req, res) =>{
     res.render("teams")
-})
+});
 
-app.use("/users", require("./routes/Routes.js"))
+app.use("/users", require("./routes/Routes.js"));
 
 app.listen(3000, err=>{
     !err? log(`Server Running on http://localhost:3000`):
